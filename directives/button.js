030042598debffc71efa28618829d66021a751ce
Olivier Louvignes <olivier@mg-crea.com>
2012-11-19 21:29:05 +0100

    fix(button): support checkboxes

--- directives/button.js
+++ directives/button.js
@@ -12,7 +12,8 @@ return {
       setter = getter.assign;
 
     // Disable bootstrap embedded button toggling
-    element.removeAttr('data-toggle');
+    if(element.attr('data-toggle') == 'button') element.removeAttr('data-toggle');
+      if(element.parent().attr('data-toggle') == 'buttons-checkbox') element.parent().removeAttr('data-toggle');
     // Watch model for changes instead
     scope.$watch(attr.ngModel, function(newValue, oldValue) {
       if(!newValue) element.removeClass('active')
