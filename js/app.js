6cd568d610a4d1fd165b4954050b58b16bdd97f3
Olivier Louvignes <olivier@mg-crea.com>
2012-11-19 20:23:15 +0100

    chore(docs): initial commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+
+$(function() {
+
+	$("html").removeClass("no-js").addClass("js");
+
+	$(document).ready(function($) {
+
+		var offset = $("header").height() + $(".navbar-fixed-top").height();
+		$(".bs-docs-sidenav").affix({offset: offset}).addClass("animated");
+
+
+		/*$("pre.prettyprint").each(function() {
+			var $this = $(this);
+			$this.text($this.html().replace('/\s+/ig', ''));
+		});*/
+		prettyPrint();
+
+
+	});
+
+});
+
+var app = angular.module('strap', ['$strap.directives']);
+
+app.controller('StrapCtrl', function($scope) {
+	$scope.button = {active: true};
+	$scope.checkbox = {left: false, middle: true, right: false};
+	$scope.datepicker = {date: ''};
+	$scope.popover = $scope.button;
+});
