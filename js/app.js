8ad50329dafc5021d1f5f3f3be7248d37bc9b5bc
Olivier Louvignes <olivier@mg-crea.com>
2012-11-20 22:28:09 +0100

    chore(docs): added typeahead docs

--- js/app.js
+++ js/app.js
@@ -26,5 +26,6 @@ app.controller('StrapCtrl', function($scope) {
 	$scope.popover = {content: 'Hello Popover', saved: false};
 	$scope.button = {active: true};
 	$scope.checkbox = {left: false, middle: true, right: false};
+	$scope.typeahead = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
 	$scope.datepicker = {date: ''};
 });
