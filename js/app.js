24dd159b37e228144d198089cd9e686075a0dc3e
Olivier Louvignes <olivier@mg-crea.com>
2012-11-22 13:44:55 +0100

    chore(docs): added timepicker docs

--- js/app.js
+++ js/app.js
@@ -28,4 +28,5 @@ app.controller('StrapCtrl', function($scope) {
 	$scope.checkbox = {left: false, middle: true, right: false};
 	$scope.typeahead = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
 	$scope.datepicker = {date: ''};
+	$scope.timepicker = {time: ''};
 });
