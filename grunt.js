c9a393c17e6348ff54b3729123861aed1ec4657e
Olivier Louvignes <olivier@mg-crea.com>
2012-11-23 14:13:29 +0100

    chore(build): added author

--- grunt.js
+++ grunt.js
@@ -10,6 +10,7 @@ module.exports = function(grunt) {
       ' * @version v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       ' * @link <%= pkg.homepage %>\n' +
+      ' * @author <%= pkg.author %>\n' +
       ' * @license MIT License, http://www.opensource.org/licenses/MIT\n' + ' */'
     },
     lint: {
