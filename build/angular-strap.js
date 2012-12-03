04685d8c06bd8cbbb71af826657ff09ee93beec4
Olivier Louvignes <olivier@mg-crea.com>
2012-12-03 02:30:40 +0100

    ver(release): bump to 0.5.4

--- build/angular-strap.js
+++ build/angular-strap.js
@@ -1,6 +1,6 @@
 /**
  * AngularStrap - Twitter Bootstrap directives for AngularJS
- * @version v0.5.3 - 2012-12-01
+ * @version v0.5.4 - 2012-12-03
  * @link http://angular-strap.github.com
  * @author Olivier Louvignes
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -144,6 +144,62 @@ angular.module('$strap.directives')
 		}
 
 	};
+
+}]);
+
+
+angular.module('$strap.directives')
+
+.directive('bsDropdown', ['$parse', '$compile', function($parse, $compile) {
+  'use strict';
+
+  var slice = Array.prototype.slice;
+
+  var template = '' +
+  '<ul class="dropdown-menu" role="menu" aria-labelledby="drop1">' +
+    '<li ng-repeat="item in items" ng-class="{divider: !!item.divider, \'dropdown-submenu\': !!item.submenu && item.submenu.length}">' +
+      '<a ng-hide="!!item.divider" tabindex="-1" href="{{item.href}}">{{item.text}}</a>' +
+    '</li>' +
+  '</ul>';
+
+  var linkSubmenu = function(items, parent, scope) {
+    var subitems, submenu, subscope;
+    for (var i = 0, l = items.length; i < l; i++) {
+      if(subitems = items[i].submenu) {
+        subscope = scope.$new(true);
+        subscope.items = subitems;
+        submenu = $compile(template)(subscope);
+        submenu = submenu.appendTo(parent.children('li:nth-child(' + (i+1) + ')'));
+        asyncLinkSubmenu(subitems, submenu, subscope);
+      }
+    }
+  };
+
+  var asyncLinkSubmenu = function() {
+    var args = slice.call(arguments);
+    setTimeout(function() {
+      linkSubmenu.apply(null, args);
+    });
+  };
+
+  return {
+    restrict: 'EA',
+    scope: true,
+    link: function postLink(scope, element, attr) {
+
+      var getter = $parse(attr.bsDropdown);
+
+      scope.items = getter(scope);
+      var dropdown = $compile(template)(scope);
+      asyncLinkSubmenu(scope.items, dropdown, scope);
+      dropdown.insertAfter(element);
+
+      element
+        .addClass('dropdown-toggle')
+        .attr('data-toggle', "dropdown");
+
+    }
+  };
 
 }]);
 
