37b7e44a10c48ed8229e47846a2d41b97560c131
Olivier Louvignes <olivier@mg-crea.com>
2012-12-22 17:01:11 +0100

    feat(buttonSelect): added buttonSelect example

--- js/app.js
+++ js/app.js
@@ -47,6 +47,7 @@ app.controller('StrapCtrl', function($scope) {
 	$scope.tooltip = {title: "Hello Tooltip<br />This is a multiline message!"};
 	$scope.popover = {content: "Hello Popover<br />This is a multiline message!", saved: false};
 	$scope.button = {active: true};
+	$scope.buttonSelect = {price: '89,99', currency: '€'};
 	$scope.checkbox = {left: false, middle: true, right: false};
 	$scope.typeahead = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
 	$scope.datepicker = {date: ''};
