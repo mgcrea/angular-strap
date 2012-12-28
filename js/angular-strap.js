a6cb29d4a744ac98e8f7deca89372b8660f3d6ad
Olivier Louvignes <olivier@mg-crea.com>
2012-12-28 23:57:30 +0100

    chore(ver): bump to v0.5.8

--- js/angular-strap.js
+++ js/angular-strap.js
@@ -1,6 +1,6 @@
 /**
  * AngularStrap - Twitter Bootstrap directives for AngularJS
- * @version v0.5.7 - 2012-12-25
+ * @version v0.5.8 - 2012-12-28
  * @link http://angular-strap.github.com
  * @author Olivier Louvignes
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -110,6 +110,8 @@ angular.module('$strap.directives')
 .directive('bsDatepicker', ['$timeout', function($timeout) {
 	'use strict';
 
+	var isTouch = 'ontouchstart' in window && !window.navigator.userAgent.match(/PhantomJS/i);
+
 	var DATE_REGEXP_MAP = {
 		'/'    : '[\\/]',
 		'-'    : '[-]',
@@ -126,16 +128,7 @@ angular.module('$strap.directives')
 		restrict: 'A',
 		require: '?ngModel',
 		link: function postLink(scope, element, attrs, controller) {
-			//console.log('postLink', this, arguments);
-
-			// If we have a controller (i.e. ngModelController) then wire it up
-			if(controller) {
-				element.on('changeDate', function(ev) {
-					scope.$apply(function () {
-						controller.$setViewValue(element.val());
-					});
-				});
-			}
+			//console.log('postLink', this, arguments); window.element = element;
 
 			var regexpForDateFormat = function(dateFormat, options) {
 				options || (options = {});
@@ -148,45 +141,68 @@ angular.module('$strap.directives')
 				return new RegExp('^' + re + '$', ['i']);
 			};
 
-			var dateFormatRegexp = regexpForDateFormat(attrs.dateFormat || 'mm/dd/yyyy'/*, {mask: !!attrs.uiMask}*/);
+			var dateFormatRegexp = isTouch ? 'yyyy/mm/dd' : regexpForDateFormat(attrs.dateFormat || 'mm/dd/yyyy'/*, {mask: !!attrs.uiMask}*/);
 
 			// Handle date validity according to dateFormat
-			controller.$parsers.unshift(function(viewValue) {
-				//console.warn('viewValue', viewValue, dateFormatRegexp,  dateFormatRegexp.test(viewValue));
-				if (!viewValue || dateFormatRegexp.test(viewValue)) {
-					controller.$setValidity('date', true);
-					return viewValue;
-				} else {
-					controller.$setValidity('date', false);
-					return undefined;
-				}
-			});
-
-			// Support add-on
-			var component = element.next('[data-toggle="datepicker"]');
-			if(component.length) {
-				component.on('click', function() { element.datepicker('show'); });
-			}
-
-			// Popover GarbageCollection
-			var $popover = element.closest('.popover');
-			if($popover) {
-				$popover.on('hide', function(e) {
-					var datepicker = element.data('datepicker');
-					if(datepicker) {
-						datepicker.picker.remove();
-						element.data('datepicker', null);
+			if(controller) {
+				controller.$parsers.unshift(function(viewValue) {
+					//console.warn('viewValue', viewValue, dateFormatRegexp,  dateFormatRegexp.test(viewValue));
+					if (!viewValue || dateFormatRegexp.test(viewValue)) {
+						controller.$setValidity('date', true);
+						return viewValue;
+					} else {
+						controller.$setValidity('date', false);
+						return undefined;
 					}
 				});
 			}
 
-			// Create datepicker
-			element.attr('data-toggle', 'datepicker');
-			//$timeout(function () { // makes the ui lag?
+			// Support add-on
+			var component = element.next('[data-toggle="datepicker"]');
+			if(component.length) {
+				component.on('click', function() { isTouch ? element.trigger('focus') : element.datepicker('show'); });
+			}
+
+			// Use native interface for touch devices
+			if(isTouch && element.prop('type') === 'text') {
+
+				element.prop('type', 'date');
+				element.on('change', function(ev) {
+					scope.$apply(function () {
+						controller.$setViewValue(element.val());
+					});
+				});
+
+			} else {
+
+				// If we have a controller (i.e. ngModelController) then wire it up
+				if(controller) {
+					element.on('changeDate', function(ev) {
+						scope.$apply(function () {
+							controller.$setViewValue(element.val());
+						});
+					});
+				}
+
+				// Popover GarbageCollection
+				var $popover = element.closest('.popover');
+				if($popover) {
+					$popover.on('hide', function(e) {
+						var datepicker = element.data('datepicker');
+						if(datepicker) {
+							datepicker.picker.remove();
+							element.data('datepicker', null);
+						}
+					});
+				}
+
+				// Create datepicker
+				element.attr('data-toggle', 'datepicker');
 				element.datepicker({
 					autoclose: true
 				});
-			//}, 0, false);
+
+			}
 
 		}
 
