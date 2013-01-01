fe3e8dd34d919f70cfcfc4737f37fd1012e15fa5
Olivier Louvignes <olivier@mg-crea.com>
2013-01-01 22:10:51 +0100

    feat(navbar): added new navbar directive

---
+++ src/directives/navbar.js
@@ -0,0 +1,30 @@
+
+angular.module('$strap.directives')
+
+.directive('bsNavbar', ['$location', function($location) {
+	'use strict';
+
+	return {
+		restrict: 'A',
+		link: function postLink($scope, element, attrs, controller) {
+			// Watch for the $location
+			$scope.$watch(function() {
+				return $location.path();
+			}, function(newValue, oldValue) {
+
+				element.find('li[data-match-route]').each(function(k, li) {
+					var $li = angular.element(li),
+						pattern = $li.data('match-route'),
+						regexp = new RegExp('^' + pattern + '$', ["i"]);
+
+					if(regexp.test(newValue)) {
+						$li.addClass('active');
+					} else {
+						$li.removeClass('active');
+					}
+
+				});
+			});
+		}
+	};
+}]);
