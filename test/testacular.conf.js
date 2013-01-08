1801b47a125df438ac694c3901d8b21278f6f308
Olivier Louvignes <olivier@mg-crea.com>
2013-01-08 13:53:27 +0100

    feat(test): added istanbul code coverage

--- test/testacular.conf.js
+++ test/testacular.conf.js
@@ -1,10 +1,10 @@
-// Testacular configuration
-// Generated on Fri Sep 07 2012 16:43:12 GMT+0200 (CEST)
-
+// Testacular configuration file
+//
+// For all available config options and default values, see:
+// https://github.com/vojtajina/testacular/blob/stable/lib/config.js#L54
 
 // base path, that will be used to resolve files and exclude
 basePath = './..';
-
 
 // list of files / patterns to load in the browser
 files = [
@@ -22,49 +22,67 @@ files = [
   'test/unit/directives/*Spec.js'
 ];
 
-
 // list of files to exclude
-exclude = [
+exclude = [];
 
-];
-
-
-// test results reporter to use
-// possible values: dots || progress
-reporter = 'progress';
+// use dots reporter, as travis terminal does not support escaping sequences
+// possible values: 'dots', 'progress', 'junit'
+// CLI --reporters progress
+reporters = ['coverage', 'progress'];
 
 
 // web server port
-port = 8080;
-
+// CLI --port 9876
+port = 9876;
 
 // cli runner port
+// CLI --runner-port 9100
 runnerPort = 9100;
 
-
 // enable / disable colors in the output (reporters and logs)
+// CLI --colors --no-colors
 colors = true;
-
 
 // level of logging
 // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+// CLI --log-level debug
 logLevel = LOG_INFO;
 
-
 // enable / disable watching file and executing tests whenever any file changes
+// CLI --auto-watch --no-auto-watch
 autoWatch = true;
-
 
 // Start these browsers, currently available:
 // - Chrome
 // - ChromeCanary
 // - Firefox
 // - Opera
-// - Safari
+// - Safari (only Mac)
 // - PhantomJS
+// - IE (only Windows)
+// CLI --browsers Chrome,Firefox,Safari
 browsers = ['PhantomJS'];
 
+// If browser does not capture in given timeout [ms], kill it
+// CLI --capture-timeout 5000
+captureTimeout = 5000;
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
+// Auto run tests on start (when browsers are captured) and exit
+// CLI --single-run --no-single-run
 singleRun = false;
+
+// report which specs are slower than 500ms
+// CLI --report-slower-than 500
+reportSlowerThan = 500;
+
+// compile coffee scripts
+preprocessors = {
+  'src/common.js': 'coverage',
+  'src/directives/*.js': 'coverage'
+  //'**/*.coffee': 'coffee'
+};
+
+coverageReporter = {
+  type : 'html',
+  dir : 'test/coverage/'
+};
