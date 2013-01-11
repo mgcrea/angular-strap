4904dac6933be0ed1977a8efec0f1d3a57037c40
Olivier Louvignes <olivier@mg-crea.com>
2013-01-11 18:21:49 +0100

    feat(button): added bsButtonsCheckbox & bsButtonsRadio directives

--- src/directives/button.js
+++ src/directives/button.js
@@ -1,46 +1,147 @@
 
 angular.module('$strap.directives')
 
-.directive('bsButton', ['$parse', function($parse) {
+.directive('bsButton', ['$parse', '$timeout', function($parse, $timeout) {
 	'use strict';
 
 	return {
 		restrict: 'A',
-		require: 'ngModel',
-		link: function postLink(scope, element, attr, ctrl) {
+		require: '?ngModel',
+		link: function postLink(scope, element, attrs, controller) {
 
-			var getter = $parse(attr.ngModel),
-				setter = getter.assign;
+			// If we have a controller (i.e. ngModelController) then wire it up
+			if(controller) {
 
-			// Disable bootstrap embedded button toggling
-			if(element.attr('data-toggle') === 'button') {
-				element.removeAttr('data-toggle');
-			}
-			if(element.parent().attr('data-toggle') === 'buttons-checkbox') {
-				element.parent().removeAttr('data-toggle');
-			}
+				// Set as single toggler if not part of a btn-group
+				if(!element.parent('[data-toggle="buttons-checkbox"], [data-toggle="buttons-radio"]').length) {
+					element.attr('data-toggle', 'button');
+				}
 
-			// Watch model for changes instead
-			scope.$watch(attr.ngModel, function(newValue, oldValue) {
-				if(!newValue) {
-					element.removeClass('active');
-				} else {
+				// Handle default state
+				if(!!scope.$eval(attrs.ngModel)) {
 					element.addClass('active');
 				}
-			});
 
-			// Click handling
-			element.on('click', function(ev) {
-				scope.$apply(function() {
-					setter(scope, !getter(scope));
+				// Watch model for changes
+				scope.$watch(attrs.ngModel, function(newValue, oldValue) {
+					if(!!newValue === !oldValue) {
+						$.fn.button.Constructor.prototype.toggle.call(button);
+					}
 				});
-			});
 
-			// Initial state
-			if(getter(scope)) {
-				element.addClass('active');
 			}
 
+			// Initialize button
+			element.button();
+
+			// Bootstrap override to handle toggling
+			var button = element.data('button');
+			button.toggle = function() {
+
+				if(!controller) {
+					return $.fn.button.Constructor.prototype.toggle.call(this);
+				}
+
+				var $parent = element.parent('[data-toggle="buttons-radio"]');
+
+				if($parent.length) {
+					element.siblings('[ng-model]').each(function(k, v) {
+						$parse($(v).attr('ng-model')).assign(scope, false);
+					});
+					scope.$digest();
+					if(!controller.$modelValue) {
+						controller.$setViewValue(!controller.$modelValue);
+						scope.$digest();
+					}
+				} else {
+					scope.$apply(function () {
+						controller.$setViewValue(!controller.$modelValue);
+					});
+				}
+
+			};
+
+			// Provide scope display functions
+			// scope._button = function(event) {
+			// 	element.button(event);
+			// };
+			// scope.loading = function() {
+			// 	element.tooltip('loading');
+			// };
+			// scope.reset = function() {
+			// 	element.tooltip('reset');
+			// };
+
+			// if(attrs.loadingText) element.click(function () {
+			// 	//var btn = $(this)
+			// 	element.button('loading')
+			// 	setTimeout(function () {
+			// 	element.button('reset')
+			// 	}, 1000)
+			// });
+
+		}
+	};
+
+}])
+
+.directive('bsButtonsCheckbox', ['$parse', function($parse) {
+	'use strict';
+	return {
+		restrict: 'A',
+		require: '?ngModel',
+		compile: function compile(tElement, tAttrs, transclude) {
+			tElement.attr('data-toggle', 'buttons-checkbox').find('a, button').each(function(k, v) {
+				$(v).attr('bs-button', '');
+			});
+		}
+	};
+
+}])
+
+.directive('bsButtonsRadio', ['$parse', function($parse) {
+	'use strict';
+	return {
+		restrict: 'A',
+		require: '?ngModel',
+		compile: function compile(tElement, tAttrs, transclude) {
+
+			tElement.attr('data-toggle', 'buttons-radio');
+
+			// Delegate to children ngModel
+			if(!tAttrs.ngModel) {
+				tElement.find('a, button').each(function(k, v) {
+					$(v).attr('bs-button', '');
+				});
+			}
+
+			return function postLink(scope, iElement, iAttrs, controller) {
+
+				// If we have a controller (i.e. ngModelController) then wire it up
+				if(controller) {
+
+					iElement
+						.find('[value]').button()
+						.filter('[value="' + scope.$eval(iAttrs.ngModel) + '"]')
+						.addClass('active');
+
+					iElement.on('click.button.data-api', function (ev) {
+						scope.$apply(function () {
+							controller.$setViewValue($(ev.target).attr('value'));
+						});
+					});
+
+					// Watch model for changes
+					scope.$watch(iAttrs.ngModel, function(newValue, oldValue) {
+						if(newValue !== oldValue) {
+							var $btn = iElement.find('[value="' + scope.$eval(iAttrs.ngModel) + '"]');
+							$.fn.button.Constructor.prototype.toggle.call($btn.data('button'));
+						}
+					});
+
+				}
+
+			};
 		}
 	};
 
