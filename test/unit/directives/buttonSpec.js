0e8c16d490851d178a1fd382624bc117e0cc2616
Olivier Louvignes <olivier@mg-crea.com>
2013-01-11 18:21:49 +0100

    feat(button): added bsButtonsCheckbox & bsButtonsRadio directives

--- test/unit/directives/buttonSpec.js
+++ test/unit/directives/buttonSpec.js
@@ -5,13 +5,30 @@ describe('button', function () {
 
   beforeEach(module('$strap.directives'));
 
+  beforeEach(function() {
+    this.addMatchers({
+      toEquals: function(expected) {
+        return angular.equals(this.actual, expected);
+      }
+    });
+  });
+
   beforeEach(inject(function ($injector, $rootScope, _$compile_, _$timeout_) {
     scope = $rootScope;
     $compile = _$compile_;
     $timeout = _$timeout_;
 
     $sandbox = $('<div id="sandbox"></div>').appendTo($('body'));
-    scope.model = {};
+
+    // Fix events not firing
+    $(document)
+      .off('click.button.data-api', '[data-toggle^=button]')
+      .on('click.button.data-api', '[data-toggle^=button]', function (e) {
+        var $btn = $(e.target);
+        if (!$btn.hasClass('btn')) $btn = $btn.closest('.btn');
+        $btn.button('toggle');
+      });
+
   }));
 
   afterEach(function() {
@@ -20,20 +37,50 @@ describe('button', function () {
   });
 
   var templates = {
-    'default': '<div class="btn" ng-model="buttonValue" data-toggle="button" bs-button>'
+    'default': {
+      scope: {buttonValue: undefined},
+      element: '<div class="btn" ng-model="buttonValue" data-toggle="button" bs-button>'
+    },
+    'checkbox': {
+      scope: {checkbox: {left: false, middle: true, right: false}},
+      element: '<div class="btn-group" bs-buttons-checkbox>'
+      +   '<button type="button" class="btn" ng-model="checkbox.left">Left</button>'
+      +   '<button type="button" class="btn" ng-model="checkbox.middle">Middle</button>'
+      +   '<button type="button" class="btn" ng-model="checkbox.right">Right</button>'
+      + '</div>'
+    },
+    'radio': {
+      scope: {radio: {left: false, middle: true, right: false}},
+      element: '<div class="btn-group" bs-buttons-radio>'
+      +   '<button type="button" class="btn" ng-model="radio.left">Left</button>'
+      +   '<button type="button" class="btn" ng-model="radio.middle">Middle</button>'
+      +   '<button type="button" class="btn" ng-model="radio.right">Right</button>'
+      + '</div>'
+    },
+    'radio-single-model': {
+      scope: {radio: 'middle'},
+      element: '<div class="btn-group" ng-model="radio" bs-buttons-radio>'
+      +   '<button type="button" class="btn" value="left">Left</button>'
+      +   '<button type="button" class="btn" value="middle">Middle</button>'
+      +   '<button type="button" class="btn" value="right">Right</button>'
+      + '</div>'
+    }
   };
 
   function compileDirective(template) {
     template = template ? templates[template] : templates['default'];
-    template = $(template).appendTo($sandbox);
-    return $compile(template)(scope);
+    angular.extend(scope, template.scope);
+    var $element = $(template.element).appendTo($sandbox);
+    $element = $compile($element)(scope);
+    scope.$digest();
+    return $element;
   }
 
   // Tests
 
-  it('should remove "data-toggle" attr for you', function () {
+  it('should add "data-toggle" attr for you', function () {
     var elm = compileDirective();
-    expect(elm.attr('data-toggle')).toBeUndefined();
+    expect(elm.attr('data-toggle')).toBe('button');
   });
 
   describe('model change to view', function () {
@@ -72,4 +119,68 @@ describe('button', function () {
       expect(scope.buttonValue).toBe(false);
     });
   });
+
+  describe('buttons checkbox', function () {
+
+    it('should handle view to model changes', function () {
+      var elm = compileDirective('checkbox');
+      elm.find('[ng-model="checkbox.left"]').trigger('click');
+      expect(scope.checkbox).toEquals({left: true, middle: true, right: false});
+      elm.find('[ng-model="checkbox.left"]').trigger('click');
+      expect(scope.checkbox).toEquals({left: false, middle: true, right: false});
+    });
+
+    it('should handle model to view changes', function () {
+      var elm = compileDirective('checkbox');
+      scope.$apply(function () {
+        scope.checkbox = {left: true, middle: false, right: true};
+      });
+      expect(elm.find('[ng-model="checkbox.left"]').hasClass('active')).toBe(true);
+      expect(elm.find('[ng-model="checkbox.middle"]').hasClass('active')).toBe(false);
+      expect(elm.find('[ng-model="checkbox.right"]').hasClass('active')).toBe(true);
+    });
+  });
+
+  describe('buttons radio', function () {
+
+    it('should handle view to model changes', function () {
+      var elm = compileDirective('radio');
+      elm.find('[ng-model="radio.left"]').trigger('click');
+      expect(scope.radio).toEquals({left: true, middle: false, right: false});
+      elm.find('[ng-model="radio.left"]').trigger('click');
+      expect(scope.radio).toEquals({left: true, middle: false, right: false});
+    });
+
+    it('should handle model to view changes', function () {
+      var elm = compileDirective('radio');
+      scope.$apply(function () {
+        scope.radio = {left: false, middle: false, right: true};
+      });
+      expect(elm.find('[ng-model="radio.left"]').hasClass('active')).toBe(false);
+      expect(elm.find('[ng-model="radio.middle"]').hasClass('active')).toBe(false);
+      expect(elm.find('[ng-model="radio.right"]').hasClass('active')).toBe(true);
+    });
+  });
+
+  describe('buttons radio single model', function () {
+
+    it('should handle view to model changes', function () {
+      var elm = compileDirective('radio-single-model');
+      elm.find('[value="left"]').trigger('click');
+      expect(scope.radio).toEquals('left');
+      elm.find('[value="left"]').trigger('click');
+      expect(scope.radio).toEquals('left');
+    });
+
+    it('should handle model to view changes', function () {
+      var elm = compileDirective('radio-single-model');
+      scope.$apply(function () {
+        scope.radio = 'right';
+      });
+      expect(elm.find('[value="left"]').hasClass('active')).toBe(false);
+      expect(elm.find('[value="middle"]').hasClass('active')).toBe(false);
+      expect(elm.find('[value="right"]').hasClass('active')).toBe(true);
+    });
+  });
+
 });
