4d6fd7f0c46bc5520cb0f1647fa15afd685f6898
Olivier Louvignes <olivier@mg-crea.com>
2013-01-17 11:52:23 +0100

    fix(release): proper v0.6.2 release

--- src/directives/alert.js
+++ src/directives/alert.js
@@ -9,21 +9,41 @@ angular.module('$strap.directives')
 		link: function postLink(scope, element, attrs) {
 
 			scope.$watch(attrs.bsAlert, function(newValue, oldValue) {
+
+				if(typeof newValue === 'undefined') {
+					if(typeof oldValue !== 'undefined') {
+						element.remove();
+					}
+					return;
+				}
+
+				// Set alert content
 				element.html((newValue.title ? '<strong>' + newValue.title + '</strong>&nbsp;' : '') + newValue.content || '');
+
 				// Compile alert content
 				$timeout(function(){
 					$compile(element.contents())(scope);
 				});
+
+				// Add proper class
 				if(newValue.type || oldValue.type) {
 					oldValue.type && element.removeClass('alert-' + oldValue.type);
 					newValue.type && element.addClass('alert-' + newValue.type);
 				}
+
+				// Setup close button
 				if(newValue.close !== false) {
 					element.prepend('<button type="button" class="close" data-dismiss="alert">&times;</button>');
 				}
+
 			}, true);
 
-			element.alert();
+			// For basic alerts
+			if(!attrs.bsAlert && attrs.close !== '0') {
+				element.prepend('<button type="button" class="close" data-dismiss="alert">&times;</button>');
+			}
+
+			element.addClass('alert').alert();
 
 			// element.on('close', function() {
 			// });
