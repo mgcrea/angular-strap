2da93173848efe23e1a4ade4fb7459baaa03a59f
michaelahlers <michael@ahlers.co>
2013-04-11 14:56:37 -0400

    Syntax error.

--- angular-strap.js
+++ angular-strap.js
@@ -1,22 +1,22 @@
 /*global define */
 
 define([
-  , './src/common'
-  , './src/directives/alert'
-  , './src/directives/button'
-  , './src/directives/buttonSelect'
-  , './src/directives/datepicker'
-  , './src/directives/dropdown'
-  , './src/directives/modal'
-  , './src/directives/navbar'
-  , './src/directives/popover'
-  , './src/directives/tab'
-  , './src/directives/timepicker'
-  , './src/directives/tooltip'
-  , './src/directives/typeahead'
+    './src/common'
+    , './src/directives/alert'
+    , './src/directives/button'
+    , './src/directives/buttonSelect'
+    , './src/directives/datepicker'
+    , './src/directives/dropdown'
+    , './src/directives/modal'
+    , './src/directives/navbar'
+    , './src/directives/popover'
+    , './src/directives/tab'
+    , './src/directives/timepicker'
+    , './src/directives/tooltip'
+    , './src/directives/typeahead'
 
-  , './vendor/bootstrap-datepicker'
-  , './vendor/bootstrap-timepicker'
+    , './vendor/bootstrap-datepicker'
+    , './vendor/bootstrap-timepicker'
 ], function () {
-  return {}
+    return {}
 })
