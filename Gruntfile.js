64a993e713a3ce5c5815cd059dd47da09bf66ea1
Olivier Louvignes <olivier@mg-crea.com>
2014-01-06 17:37:49 +0100

    chore(release): add readme

---
+++ Gruntfile.js
@@ -0,0 +1,496 @@
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  // require('time-grunt')(grunt);
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Project meta
+    pkg: require('./package.json'),
+    bower: require('./bower.json'),
+    meta: {
+      banner: '/**\n' +
+      ' * <%= pkg.name %>\n' +
+      ' * @version v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      ' * @link <%= pkg.homepage %>\n' +
+      ' * @author <%= pkg.author %>\n' +
+      ' * @license MIT License, http://www.opensource.org/licenses/MIT\n' +
+      ' */\n'
+    },
+
+    // Project settings
+    yo: {
+      // Configurable paths
+      src: require('./bower.json').appPath || 'src',
+      dist: 'dist',
+      docs: 'docs',
+      pages: 'pages'
+    },
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      js: {
+        files: ['{.tmp,<%= yo.src %>}/scripts/{,*/}*.js'],
+        tasks: ['newer:jshint:all']
+      },
+      jsTest: {
+        files: ['test/spec/{,*/}*.js'],
+        tasks: ['newer:jshint:test', 'karma']
+      },
+      styles: {
+        options: {
+          spawn: false
+        },
+        files: ['{docs,<%= yo.src %>}/styles/{,*/}*.less'],
+        tasks: ['less:dev', 'autoprefixer']
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '{docs,<%= yo.src %>}/{,*/}{,docs/}*.html',
+          '{docs,.tmp,<%= yo.src %>}/{,*/}*.css',
+          '{docs,.tmp,<%= yo.src %>}/{,*/}*.js',
+          '{docs,<%= yo.src %>}/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '.tmp',
+            'docs',
+            '<%= yo.src %>'
+          ]
+        }
+      },
+      test: {
+        options: {
+          port: 9001,
+          base: [
+            '.tmp',
+            'test',
+            '<%= yo.src %>'
+          ]
+        }
+      },
+      dist: {
+        options: {
+          base: '<%= yo.dist %>'
+        }
+      }
+    },
+
+    // Make sure code styles are up to par and there are no obvious mistakes
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= yo.src %>/scripts/{,*/}*.js'
+      ],
+      test: {
+        options: {
+          jshintrc: 'test/.jshintrc'
+        },
+        src: ['test/spec/{,*/}*.js']
+      }
+    },
+
+    // Empties folders to start fresh
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= yo.dist %>/*',
+            '!<%= yo.dist %>/.git*'
+          ]
+        }]
+      },
+      docs: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= yo.pages %>/*',
+            '!<%= yo.pages %>/.git*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+
+    // Compile less stylesheets
+    less: {
+      options: {
+      },
+      dev: {
+        options: {
+          dumpLineNumbers: 'comments',
+        },
+        files: [{
+          src: '<%= yo.docs %>/styles/main.less',
+          dest: '.tmp/styles/main.css'
+        }]
+      },
+      docs: {
+        options: {
+          cleancss: true,
+          report: 'gzip'
+        },
+        files: [{
+          src: '<%= yo.docs %>/styles/main.less',
+          dest: '.tmp/styles/main.css'
+        }]
+      }
+    },
+
+    // Add vendor prefixed styles
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions']
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/styles/',
+          src: '{,*/}*.css',
+          dest: '.tmp/styles/'
+        }]
+      }
+    },
+
+    // Reads HTML for usemin blocks to enable smart builds that automatically
+    // concat, minify and revision files. Creates configurations in memory so
+    // additional tasks can operate on them
+    useminPrepare: {
+      html: '<%= yo.docs %>/index.html',
+      options: {
+        dest: '<%= yo.pages %>'
+      }
+    },
+
+    // Performs rewrites based on rev and the useminPrepare configuration
+    usemin: {
+      html: '<%= yo.pages %>/index.html',
+      // css: ['<%= yo.pages %>/styles/{,*/}*.css'],
+      options: {
+        assetsDirs: ['<%= yo.pages %>']
+      }
+    },
+
+    // The following *-min tasks produce minified files in the dist folder
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= yo.src %>/images',
+          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          dest: '<%= yo.dist %>/images'
+        }]
+      }
+    },
+    svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= yo.src %>/images',
+          src: '{,*/}*.svg',
+          dest: '<%= yo.dist %>/images'
+        }]
+      }
+    },
+
+    // Minify html files
+    htmlmin: {
+      options: {
+        collapseWhitespace: false,
+        removeComments: false
+      },
+      docs: {
+        files: [{
+          expand: true,
+          cwd: '<%= yo.docs %>',
+          src: ['*.html', 'views/{,*/}*.html'],
+          dest: '<%= yo.pages %>'
+        }]
+      }
+    },
+
+    // Replace Google CDN references
+    cdnify: {
+      dist: {
+        html: ['<%= yo.pages %>/*.html']
+      }
+    },
+
+    // Renames files for browser caching purposes
+    rev: {
+      dist: {
+        files: {
+          src: [
+            '<%= yo.pages %>/scripts/{,*/}*.js',
+            '<%= yo.pages %>/styles/{,*/}*.css',
+            '<%= yo.pages %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+            '<%= yo.pages %>/styles/fonts/*'
+          ]
+        }
+      }
+    },
+
+    // Copies remaining files to places other tasks can use
+    copy: {
+      fonts: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: 'bower_components/font-awesome',
+          dest: '<%= yo.pages %>',
+          src: [
+            'fonts/*'
+          ]
+        }]
+      },
+      docs: {
+        files: [{
+          expand: true,
+          dot: true,
+          dest: '<%= yo.pages %>',
+          src: [
+            // 'bower_components/**/*',
+            'fonts/*'
+          ]
+        }]
+      }
+    },
+
+    // Run some tasks in parallel to speed up the build process
+    concurrent: {
+      docs: [
+        'less:docs',
+        'concat:generated',
+        'uglify:generated',
+        'cssmin:generated',
+        'htmlmin'
+      ],
+      server: [
+        'less:dev'
+      ],
+      test: [
+        'less:dev'
+      ],
+      dist: [
+        'less:dist',
+        'imagemin',
+        'svgmin',
+        'htmlmin'
+      ]
+    },
+
+    concat: {
+      // generated: {
+      //   options: {
+      //     banner: '(function(window, document, $, undefined) {\n\'use strict\';\n',
+      //     footer: '\n})(window, document, window.jQuery);\n'
+      //   }
+      // },
+      dist: {
+        options: {
+          // Replace all 'use strict' statements in the code with a single one at the top
+          banner: '(function(window, document, $, undefined) {\n\'use strict\';\n',
+          footer: '\n})(window, document, window.jQuery);\n',
+          process: function(src, filepath) {
+            return '// Source: ' + filepath + '\n' +
+              src.replace(/(^|\n)[ \t]*('use strict'|"use strict");?\s*/g, '$1');
+          }
+        },
+        files: {
+          '<%= yo.dist %>/<%= pkg.name %>.js': [
+            '<%= yo.src %>/common.js',
+            '<%= yo.src %>/{,*/}*.js'
+          ]
+        }
+      },
+      banner: {
+        options: {
+          banner: '<%= meta.banner %>',
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= yo.dist %>',
+          src: '*',
+          dest: '<%= yo.dist %>'
+        }]
+      },
+      docs: {
+        options: {
+          banner: '<%= meta.banner %>',
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= yo.pages %>',
+          src: ['scripts/{demo,docs,angular-strap}*', 'styles/{main}*'],
+          dest: '<%= yo.pages %>'
+        }]
+      }
+    },
+
+    // Allow the use of non-minsafe AngularJS files. Automatically makes it
+    // minsafe compatible so Uglify does not destroy the ng references
+    ngmin: {
+      dist: {
+        files: [{
+          src: '<%= yo.dist %>/<%= pkg.name %>.js',
+          dest: '<%= yo.dist %>/<%= pkg.name %>.js'
+        }]
+      },
+      modules: {
+        files: [{
+          expand: true,
+          flatten: true,
+          cwd: '<%= yo.src %>',
+          src: '{,*/}*.js',
+          dest: '<%= yo.dist %>/modules'
+        }]
+      },
+      docs: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/concat/scripts',
+          src: '*.js',
+          dest: '.tmp/concat/scripts'
+        }]
+      }
+    },
+
+    ngtemplates:  {
+      docs: {
+        options:  {
+          module: 'mgcrea.ngStrapDocs',
+          usemin: 'scripts/docs.min.js'
+        },
+        cwd: '<%= yo.src %>',
+        src: [
+          '{,*/}docs/*.html',
+          // '<%= yo.src %>/{,*/}docs/*.html',
+          // '<%= yo.docs %>/views/{,*/}*.html'
+        ],
+        dest: '.tmp/ngtemplates/scripts/docs.js'
+      }
+    },
+
+    uglify: {
+      dist: {
+        options: {
+          report: 'gzip',
+          sourceMap: '<%= yo.dist %>/<%= pkg.name %>.min.map'
+        },
+        files: [{
+          dest: '<%= yo.dist %>/<%= pkg.name %>.min.js',
+          src: '<%= yo.dist %>/<%= pkg.name %>.js'
+        }]
+      }
+    },
+
+    // Test settings
+    karma: {
+      options: {
+        configFile: 'test/karma.conf.js',
+        browsers: ['PhantomJS']
+      },
+      unit: {
+        singleRun: true
+      },
+      server: {
+        autoWatch: true
+      }
+    }
+  });
+
+
+  grunt.registerTask('serve', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'concurrent:server',
+      'autoprefixer',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+
+  grunt.registerTask('test', [
+    'clean:server',
+    // 'concurrent:test',
+    // 'autoprefixer',
+    'connect:test',
+    'karma:unit'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'concat:dist',
+    'ngmin:dist',
+    'ngmin:modules',
+    'uglify:dist',
+  ]);
+
+  grunt.registerTask('docs', [
+    'clean:docs',
+    'useminPrepare',
+    // 'concurrent:docs',
+    'less:docs',
+    'autoprefixer',
+    'htmlmin:docs',
+    'ngtemplates:docs',
+    'concat:generated',
+    'ngmin:docs',
+    'copy:fonts',
+    'copy:docs',
+    'cssmin:generated',
+    'uglify:generated',
+    'concat:docs',
+    'rev',
+    'usemin'
+  ]);
+
+  grunt.registerTask('default', [
+    'newer:jshint',
+    'test',
+    'build'
+  ]);
+
+};
---
+++ README.md
@@ -0,0 +1,93 @@
+# [AngularStrap v2](http://mgcrea.github.io/angular-strap) [![Build Status](https://secure.travis-ci.org/mgcrea/angular-strap.png?branch=master)](http://travis-ci.org/#!/mgcrea/angular-strap)
+
+AngularStrap is a set of native directives that enables seamless integration of [Twitter Bootstrap 3.0+](https://twitter.github.com/bootstrap) into your [AngularJS 1.2.0+](https://github.com/angular/angular.js) app.
+
+
+
+## Documentation and examples
+
++ Check the [documentation](http://mgcrea.github.io/angular-strap) and [changelog](https://github.com/mgcrea/angular-strap/wiki/Changelog).
+
++ Use & fork the available [plunkers](https://github.com/mgcrea/angular-strap/wiki/Plunkers) to test a directive or report an issue.
+
+
+
+## Quick start
+
++ Include the required libraries (cdn/local)
+
+>
+``` html
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular.min.js"></script>
+<script src="//rawgithub.com/mgcrea/angular-strap/master/dist/angular-strap.min.js"></script>
+```
+
++ Inject the `ngStrap` module into your app
+
+>
+``` javascript
+angular.module('myApp', ['ngStrap']);
+```
+
+
+## Developers
+
+Clone the repo, `git clone git://github.com/mgcrea/angular-strap.git`, [download the latest release](https://github.com/mgcrea/angular-strap/zipball/master) or install with bower `bower install angular-strap`.
+
+AngularStrap is tested with `karma` against the latest stable release of AngularJS.
+
+>
+	$ npm install grunt-cli --global
+	$ npm install --dev
+	$ grunt test
+
+You can build the latest version using `grunt`.
+
+>
+	$ grunt build
+
+You can quickly hack around (the docs) with:
+
+>
+	$ grunt serve
+
+
+
+## Contributing
+
+Please submit all pull requests the against master branch. If your unit test contains JavaScript patches or features, you should include relevant unit tests. Thanks!
+
+
+
+## Authors
+
+**Olivier Louvignes**
+
++ http://olouv.com
++ http://github.com/mgcrea
+
+
+
+## Copyright and license
+
+	The MIT License
+
+	Copyright (c) 2012 Olivier Louvignes
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
