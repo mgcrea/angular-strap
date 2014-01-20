e50538ae0494600195de05c70e7070238c465a02
Olivier Louvignes <olivier@mg-crea.com>
2014-01-20 21:51:04 +0100

    chore(gruntfile): fix jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -476,6 +476,7 @@ module.exports = function (grunt) {
 
     coveralls: {
       options: {
+        /*jshint camelcase: false */
         coverage_dir: 'test/coverage/PhantomJS 1.9.2 (Linux)/'
       }
     }
