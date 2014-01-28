9166fb9145777c5266f9de1b87463fd1d44e5ecd
Olivier Louvignes <olivier@mg-crea.com>
2014-01-28 12:02:44 +0100

    chore(grunt): remove useless line

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,6 @@ module.exports = function (grunt) {
 
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
-  grunt.loadNpmTasks('grunt-nginclude');
 
   // Time how long tasks take. Can help when optimizing build times
   // require('time-grunt')(grunt);
