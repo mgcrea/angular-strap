b210c1ec31ed4bc392f515e1deaa750a50f63539
Olivier Louvignes <olivier@mg-crea.com>
2014-01-28 12:01:12 +0100

    feat(timepicker): provide $timepicker module

---
+++ src/timepicker/docs/timepicker.demo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('mgcrea.ngStrapDocs')
+
+.controller('TimepickerDemoCtrl', function($scope, $http) {
+  $scope.time = new Date(1970, 0, 1, 10, 30);
+  $scope.selectedTimeAsNumber = 10 * 36e5;
+  $scope.sharedDate = new Date(new Date().setMinutes(0));
+});
