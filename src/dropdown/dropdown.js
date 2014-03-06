6934aae51d1b38bc2ca47acb19e4555dd1bd01a8
Olivier Louvignes <olivier@mg-crea.com>
2014-03-06 15:27:36 +0100

    fix(dropdown): prevent route reload when ngClick is used

--- src/dropdown/dropdown.js
+++ src/dropdown/dropdown.js
@@ -16,7 +16,7 @@ angular.module('mgcrea.ngStrap.dropdown', ['mgcrea.ngStrap.tooltip'])
       delay: 0
     };
 
-    this.$get = function($window, $tooltip) {
+    this.$get = function($window, $rootScope, $tooltip) {
 
       var bodyEl = angular.element($window.document.body);
       var matchesSelector = Element.prototype.matchesSelector || Element.prototype.webkitMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.msMatchesSelector || Element.prototype.oMatchesSelector;
@@ -27,6 +27,7 @@ angular.module('mgcrea.ngStrap.dropdown', ['mgcrea.ngStrap.tooltip'])
 
         // Common vars
         var options = angular.extend({}, defaults, config);
+        var scope = $dropdown.$scope = options.scope && options.scope.$new() || $rootScope.$new();
 
         $dropdown = $tooltip(element, options);
 
