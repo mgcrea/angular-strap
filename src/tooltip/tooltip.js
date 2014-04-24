774f013175fff7f9be879dc2be6b26fce1725631
Ben Doherty <ben@bendoherty.net>
2014-04-24 00:02:13 -0500

    feat(tooltip) Add target element option

--- src/tooltip/tooltip.js
+++ src/tooltip/tooltip.js
@@ -18,7 +18,8 @@ angular.module('mgcrea.ngStrap.tooltip', ['mgcrea.ngStrap.helpers.dimensions'])
       show: false,
       title: '',
       type: '',
-      delay: 0
+      delay: 0,
+      target: null
     };
 
     this.$get = function($window, $rootScope, $compile, $q, $templateCache, $http, $animate, $timeout, dimensions, $$rAF) {
@@ -130,6 +131,12 @@ angular.module('mgcrea.ngStrap.tooltip', ['mgcrea.ngStrap.helpers.dimensions'])
               options.trigger === 'focus' ? element[0].focus() : $tooltip.show();
             });
           }
+
+		  // Options: target
+		  if(options.target) {
+			  options.target = (angular.isElement(options.target)) ? options.target : findElement(options.target)[0];
+			  //console.log('element',options.target);
+		  }
 
         };
 
@@ -299,9 +306,9 @@ angular.module('mgcrea.ngStrap.tooltip', ['mgcrea.ngStrap.helpers.dimensions'])
 
         function getPosition() {
           if(options.container === 'body') {
-            return dimensions.offset(element[0]);
+            return dimensions.offset(options.target || element[0]);
           } else {
-            return dimensions.position(element[0]);
+            return dimensions.position(options.target || element[0]);
           }
         }
 
@@ -398,7 +405,7 @@ angular.module('mgcrea.ngStrap.tooltip', ['mgcrea.ngStrap.helpers.dimensions'])
 
         // Directive options
         var options = {scope: scope};
-        angular.forEach(['template', 'contentTemplate', 'placement', 'container', 'delay', 'trigger', 'keyboard', 'html', 'animation', 'type'], function(key) {
+        angular.forEach(['template', 'contentTemplate', 'placement', 'container', 'delay', 'trigger', 'keyboard', 'html', 'animation', 'type','target'], function(key) {
           if(angular.isDefined(attr[key])) options[key] = attr[key];
         });
 
