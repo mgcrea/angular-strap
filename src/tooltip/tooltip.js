9e175ded5e1df4829dbfd4665c1213bbb17d39a9
Olivier Louvignes <olivier@mg-crea.com>
2014-08-24 23:59:22 +0200

    fix(tooltip): use angular element (fixes #982)

--- src/tooltip/tooltip.js
+++ src/tooltip/tooltip.js
@@ -131,7 +131,7 @@ angular.module('mgcrea.ngStrap.tooltip', ['mgcrea.ngStrap.helpers.dimensions'])
 
           // Options: target
           if(options.target) {
-            options.target = angular.isElement(options.target) ? options.target : findElement(options.target)[0];
+            options.target = angular.isElement(options.target) ? options.target : findElement(options.target);
           }
 
           // Options: show
