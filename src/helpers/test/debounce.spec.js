6aa5600faca3a3fd2e99be23862773a3b3b71ff3
just-boris <just-boris@yandex-team.ru>
2014-08-29 19:40:03 +0400

    fix(debounce): use $timeout for deferring tasks

--- src/helpers/test/debounce.spec.js
+++ src/helpers/test/debounce.spec.js
@@ -2,13 +2,14 @@
 
 describe('debounce', function() {
 
-  var $compile, scope, debounce, throttle;
+  var $compile, $timeout, scope, debounce, throttle;
 
   beforeEach(module('mgcrea.ngStrap.helpers.debounce'));
 
-  beforeEach(inject(function(_$rootScope_, _$compile_, _debounce_, _throttle_) {
+  beforeEach(inject(function(_$rootScope_, _$compile_, _$timeout_, _debounce_, _throttle_) {
     scope = _$rootScope_;
     $compile = _$compile_;
+    $timeout = _$timeout_;
     debounce = _debounce_;
     throttle = _throttle_;
   }));
@@ -17,34 +18,30 @@ describe('debounce', function() {
 
   describe('debounce', function() {
 
-    it('should correctly debounce a function', function(done) {
+    it('should correctly debounce a function', function() {
       var counter = 0;
       var incr = function() { counter++; };
       var debouncedIncr = debounce(incr, 12);
       debouncedIncr();
       debouncedIncr();
       expect(counter).toBe(0);
-      setTimeout(function() {
-        expect(counter).toBe(1);
-        done();
-      }, 12);
+      $timeout.flush();
+      expect(counter).toBe(1);
     });
 
   });
 
   describe('throttle', function() {
 
-    it('should correctly throttle a function', function(done) {
+    it('should correctly throttle a function', function() {
       var counter = 0;
       var incr = function() { counter++; };
       var throttledIncr = throttle(incr, 12);
       throttledIncr();
       throttledIncr();
       expect(counter).toBe(1);
-      setTimeout(function() {
-        expect(counter).toBe(2);
-        done();
-      }, 12);
+      $timeout.flush();
+      expect(counter).toBe(2);
     });
 
   });
