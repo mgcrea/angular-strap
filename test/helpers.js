3d6fd76c4b09aec2bda396c7200f5d5a561f31be
Vitor Fernandes <vmlf01@gmail.com>
2014-10-28 10:06:17 +0000

    test(tooltip): add resource allocation tests
    Add function countScopes() to the test/helpers.js that allows counting the number of existing scopes.
    Add some basic allocation tests to tooltip to check the number of created scopes.

--- test/helpers.js
+++ test/helpers.js
@@ -24,3 +24,25 @@ beforeEach(function() {
     }
   });
 });
+
+/*
+ * Counts the number of scopes beginning with the 
+ * passed in scope s.
+ * It counts scopes recursively by traversing each 
+ * of the child scopes.
+ * 
+ * returns the number of scopes found
+ *
+ * s -> scope to begin with
+ * count -> current scope count, should begin with 0
+ */
+function countScopes(s, count) {
+  if (s != null) {
+    s = s.$$childHead;
+    while (s != null) {
+      count = countScopes(s, count);
+      s = s.$$nextSibling;
+    }
+  }
+  return ++count;
+}
