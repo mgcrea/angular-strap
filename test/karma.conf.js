03ca55b1d527f78f538dd65f8996e1ab3a126aa2
Olivier Louvignes <olivier@mg-crea.com>
2014-11-13 11:16:45 +0100

    test(karma): setup SauceLabs testing

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -46,6 +46,43 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
+    // For more browsers on Sauce Labs see:
+    // https://saucelabs.com/docs/platforms/webdriver
+    customLaunchers: {
+      'SL_Chrome': {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+      },
+      'SL_Firefox': {
+        base: 'SauceLabs',
+        browserName: 'firefox'
+      },
+      'SL_Safari': {
+        base: 'SauceLabs',
+        browserName: 'safari',
+        platform: 'OS X 10.9',
+        version: '7'
+      },
+      'SL_IE_9': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 7',
+        version: '9'
+      },
+      'SL_IE_10': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 7',
+        version: '10'
+      },
+      'SL_IE_11': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 8.1',
+        version: '11'
+      }
+    },
+
     // Start these browsers, currently available:
     // - Chrome
     // - ChromeCanary
@@ -68,6 +105,7 @@ module.exports = function(config) {
       'karma-jasmine',
       'karma-phantomjs-launcher',
       'karma-chrome-launcher',
+      'karma-sauce-launcher',
       'karma-coverage'
     ],
 
