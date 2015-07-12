94a66e99283f516e74b8249267d03c4cb85cff34
Olivier Louvignes <olivier@mg-crea.com>
2015-07-13 01:40:56 +0200

    docs(global): 1.4 release related docs fixes

--- src/modal/docs/modal.demo.js
+++ src/modal/docs/modal.demo.js
@@ -13,11 +13,12 @@ angular.module('mgcrea.ngStrapDocs')
 
   // Controller usage example
   //
-  function MyModalController($scope, $q) {
-    console.warn('in');
-    $scope.foo = 'bar';
+  function MyModalController($scope) {
+    $scope.title = 'Some Title';
+    $scope.content = 'Hello Modal<br />This is a multiline message from a controller!';
   }
-  var myModal = $modal({title: 'Title', content: 'Hello Modal<br />This is a multiline message!', controller: MyModalController, template: 'modal/docs/modal.demo.tpl.html', show: false});
+  MyModalController.$inject = ['$scope'];
+  var myModal = $modal({controller: MyModalController, templateUrl: 'modal/docs/modal.demo.tpl.html', show: false});
   $scope.showModal = function() {
     myModal.$promise.then(myModal.show);
   };
