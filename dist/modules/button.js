8ebfabaefbe09b09cb9010c43d7e192d5b3c7f5e
Roman Ostolosh <roman.ostolosh@kreditech.com>
2016-08-05 13:02:24 +0300

    $bsCompile. Updated dist

--- dist/modules/button.js
+++ dist/modules/button.js
@@ -1,6 +1,6 @@
 /**
  * angular-strap
- * @version v2.3.9 - 2016-06-10
+ * @version v2.3.9 - 2016-08-05
  * @link http://mgcrea.github.io/angular-strap
  * @author Olivier Louvignes <olivier@mg-crea.com> (https://github.com/mgcrea)
  * @license MIT License, http://www.opensource.org/licenses/MIT
