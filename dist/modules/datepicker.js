473db816e495647170d4e53d513dcd9e0152b99b
Michael Gardner <Michael_Gardner@skillsoft.com>
2017-08-28 13:31:35 -0400

    chore(release): Internal release

--- dist/modules/datepicker.js
+++ dist/modules/datepicker.js
@@ -1,6 +1,6 @@
 /**
  * angular-strap
- * @version v2.3.12 - 2017-06-29
+ * @version v2.3.12 - 2017-08-28
  * @link http://mgcrea.github.io/angular-strap
  * @author Olivier Louvignes <olivier@mg-crea.com> (https://github.com/mgcrea)
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -433,7 +433,11 @@ angular.module('mgcrea.ngStrap.datepicker', [ 'mgcrea.ngStrap.helpers.dateParser
         } else {
           date = new Date(modelValue);
         }
-        controller.$dateValue = dateParser.timezoneOffsetAdjust(date, options.timezone);
+        if (options.timezone === 'UTC') {
+          controller.$dateValue = date;
+        } else {
+          controller.$dateValue = dateParser.timezoneOffsetAdjust(date, options.timezone);
+        }
         return getDateFormattedString();
       });
       controller.$render = function() {
